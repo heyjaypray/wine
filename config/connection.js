f952978d4db827eb908f7d13463736c7eef56146
Justin <hey@jaypray.com>
2018-02-16 21:26:04 -0700

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "wine_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "wine_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
