bb8871fdc9b1d0566c42d424a22be05ddf43e761
Justin <hey@jaypray.com>
2018-02-19 11:28:33 -0700

    added css

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,3 @@ DROP DATABASE IF EXISTS wine_db;
 
 CREATE DATABASE wine_db;
 USE wine_db;
-
-CREATE TABLE wine
-(
-	id int NOT NULL AUTO_INCREMENT,
-	wine_name varchar(255) NOT NULL,
-	drank boolean,
-	PRIMARY KEY (id)
-);
